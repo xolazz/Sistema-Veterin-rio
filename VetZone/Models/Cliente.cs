334161bf508e0aebfa12985dc25fc5536f9b33a8
Nicolas <nicolas.tgeremias@gmail.com>
2025-06-03 23:32:05 -0300

    Backup para inserir SQLite

---
+++ VetZone/Models/Cliente.cs
@@ -0,0 +1,15 @@
+namespace VetZone.Models;
+
+public class Cliente : ContentPage
+{
+	public Cliente()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
--- VetZone/Pages/EspeciesPage.xaml.cs
+++ VetZone/Pages/EspeciesPage.xaml.cs
@@ -7,24 +7,9 @@ namespace VetZone
             InitializeComponent();
         }
 
-        private async void OnEspecieClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new CadastroEspeciePage());
-        }
-
         private async void OnAdicionarEspecieClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new CadastroEspeciePage());
-        }
-
-        private async void OnEditarEspecieClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new CadastroEspeciePage());
-        }
-
-        private void OnExcluirEspecieClicked(object sender, EventArgs e)
-        {
-            // Código para excluir cliente futuramente
         }
 
         private void OpenFlyoutMenu(object sender, EventArgs e)
